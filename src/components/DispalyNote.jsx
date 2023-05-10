180abf3c029e0b0ecdb208fa90237fb907bccbc7
Badreddine <boudaoud.badreddine@gmail.com>
2023-05-10 20:02:19 +0200

    updated display note component

--- src/components/DispalyNote.jsx
+++ src/components/DispalyNote.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "../styles/displayNoteStyles/displayNote.css";
-function DisplayNote({ currentBook, allNotes, bookId }) {
+function DisplayNote({ currentBook, allNotes, bookId, darkMode }) {
    const [displayNote, setDispalyNote] = useState();
    useEffect(() => {
       if (currentBook) {
@@ -13,28 +13,37 @@ function DisplayNote({ currentBook, allNotes, bookId }) {
       setDispalyNote(notes);
    }, [currentBook, allNotes]);
    return (
-      <section className="display-note-container">
-         {displayNote ? (
-            <div>
-               <p className="number-of-notes">
-                  {displayNote.length
-                     ? "Notes (" + displayNote.length + ")"
-                     : "You do not have any note for this book"}
-               </p>
+      <main className={darkMode ? "display-note-container dark-mode" : "display-note-container"}>
+         {currentBook ? (
+            <div className="selected-book-note">
+               <p className="number-of-notes">{`Notes (${displayNote.length})`}</p>
                {displayNote.length ? (
                   <div className="last-note">
                      <div className="last-note-date-page">
                         Last note
-                        <p>{displayNote[displayNote.length - 1].date}</p>
-                        <p>Page {displayNote[displayNote.length - 1].page}</p>
+                        <p>{displayNote[0].date}</p>
+                        <p>{displayNote[0].time}</p>
+                        <p>Page {displayNote[0].page}</p>
                      </div>
-                     <p className="last-note-text">{displayNote[displayNote.length - 1].text}</p>
-                     <Link>Edit/See all notes</Link>
+                     <p className="last-note-text">{displayNote[0].text}</p>
+                     <Link
+                        state={{ bookId: bookId, currentBook: currentBook }}
+                        style={
+                           darkMode
+                              ? { color: "#66b8ff", textDecoration: "none", cursor: "auto" }
+                              : { color: "#003147", textDecoration: "none", cursor: "auto" }
+                        }
+                        to={bookId}
+                     >
+                        <p className="see-all-notes">See all notes</p>
+                     </Link>
                   </div>
                ) : null}
             </div>
-         ) : null}
-      </section>
+         ) : (
+            <p className="unselected-book-note">Select a book to see if you wrote a note.</p>
+         )}
+      </main>
    );
 }
 
