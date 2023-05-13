c0cd025ba8b123d0817d704c37dc485d481ae38a
Badreddine <boudaoud.badreddine@gmail.com>
2023-05-13 23:15:25 +0200

    updated app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,64 @@
-import { createContext, useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
-import Home from "./components/Home";
-import MyBooks from "./components/MyBooks";
-import Searched from "./components/Searched";
-import BooksReadingNow from "./components/BooksReadingNow";
-import BooksFavorite from "./components/BooksFavorite";
-import BooksToRead from "./components/BooksToRead";
-import BooksHaveRead from "./components/BooksHaveRead";
-import MyNotes from "./components/MyNotes";
+// import Home from "./components/Home";
+// import MyBooks from "./components/MyBooks";
+// import Searched from "./components/Searched";
+// import BooksReadingNow from "./components/BooksReadingNow";
+// import BooksFavorite from "./components/BooksFavorite";
+// import BooksToRead from "./components/BooksToRead";
+// import BooksHaveRead from "./components/BooksHaveRead";
+// import MyNotes from "./components/MyNotes";
+// import AllNotesBook from "./components/AllNotesBook";
+import AnimatedRoutes from "./components/AnimatedRoutes";
+import { createContext, useState } from "react";
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import AllNotesBook from "./components/AllNotesBook";
+import { BrowserRouter as Router /*Routes, Route*/ } from "react-router-dom";
 
-export const readingNowBooksContext = createContext();
+export const currentReadingContext = createContext();
 export const favoriteBooksContext = createContext();
 export const toReadBooksContext = createContext();
 export const haveReadBooksContext = createContext();
 
+export const getLocalStorage = (name) => {
+   if (localStorage.getItem(name)) {
+      return JSON.parse(localStorage.getItem(name));
+   } else {
+      return [];
+   }
+};
+
 function App() {
    const [darkMode, setDarkMode] = useState(false);
-   const [data, setData] = useState();
-   const [bookDetails, setBookDetails] = useState();
 
-   const getLocalStorage = (name) => {
-      if (localStorage.getItem(name)) {
-         return JSON.parse(localStorage.getItem(name));
-      } else {
-         return [];
-      }
-   };
+   // const [data, setData] = useState();
+   // const [bookDetails, setBookDetails] = useState();
 
-   const [readingNowBooks, setReadingNowBooks] = useState(() =>
-      getLocalStorage("reading now books")
+   // const getLocalStorage = (name) => {
+   //    if (localStorage.getItem(name)) {
+   //       return JSON.parse(localStorage.getItem(name));
+   //    } else {
+   //       return [];
+   //    }
+   // };
+
+   const [currentReadingBooks, setCurrentReadingBooks] = useState(() =>
+      getLocalStorage("current reading books")
    );
    const [favoriteBooks, setFavoriteBooks] = useState(() => getLocalStorage("favorite books"));
    const [toReadBooks, setToReadBooks] = useState(() => getLocalStorage("to read books"));
    const [haveReadBooks, setHaveReadBooks] = useState(() => getLocalStorage("have read books"));
-   const [allBooks, setAllBooks] = useState(() => getLocalStorage("all books"));
-   const [allNotes, setAllNotes] = useState(() => getLocalStorage("all notes"));
+   // const [allBooks, setAllBooks] = useState(() => getLocalStorage("all books"));
+   // const [allNotes, setAllNotes] = useState(() => getLocalStorage("all notes"));
 
    return (
       <Router>
          <div className="App">
-            <readingNowBooksContext.Provider value={{ readingNowBooks, setReadingNowBooks }}>
+            <currentReadingContext.Provider value={{ currentReadingBooks, setCurrentReadingBooks }}>
                <favoriteBooksContext.Provider value={{ favoriteBooks, setFavoriteBooks }}>
                   <toReadBooksContext.Provider value={{ toReadBooks, setToReadBooks }}>
                      <haveReadBooksContext.Provider value={{ haveReadBooks, setHaveReadBooks }}>
                         <Header darkMode={darkMode} setDarkMode={setDarkMode}></Header>
-                        <Routes>
+                        {/* <Routes>
                            <Route
                               path="/"
                               element={
@@ -143,11 +153,12 @@ function App() {
                                  />
                               }
                            />
-                        </Routes>
+                        </Routes> */}
+                        <AnimatedRoutes darkMode={darkMode} />
                      </haveReadBooksContext.Provider>
                   </toReadBooksContext.Provider>
                </favoriteBooksContext.Provider>
-            </readingNowBooksContext.Provider>
+            </currentReadingContext.Provider>
          </div>
       </Router>
    );
